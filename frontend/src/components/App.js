492b740fc0772f58498163abd308615bcd12abe5
Ivan123321789 <vetluga11@yandex.ru>
2023-06-28 20:28:07 +0300

    исправлена ошибка определения владельца карточки

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -55,6 +55,8 @@ function App() {
   
   function handleLikeClick(card) {
     console.log(card);
+    console.log(card.likes._id);
+    console.log(currentUser._id);
     // const isLiked = card.likes.some(i => i._id === currentUser._id);
     const isLiked = card.likes.some(i => i === currentUser._id);
     api.changeLikeCardStatus(card._id, !isLiked)
